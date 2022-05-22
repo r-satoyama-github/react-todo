de7cfdef98340071f4713424bd3724ce103c8c36
r-satoyama-github <81520511+r-satoyama-github@users.noreply.github.com>
2022-05-22 23:49:31 +0900

    add,delete,complete,back完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,78 @@
 import "./styles.css";
-import React from "react";
+import React, { useState } from "react";
 
 export const App = () => {
+  const [inCompleateTodos, setIncCompleateTodos] = useState([]);
+  const [compleateTodos, setCompleateTodos] = useState([]);
+  const [todoText, setTodoText] = useState("");
+
+  const onChangeText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...inCompleateTodos, todoText];
+    setIncCompleateTodos(newTodos);
+    setTodoText("");
+  };
+
+  const onClickDelete = (index) => {
+    const newTodos = [...inCompleateTodos];
+    newTodos.splice(index, 1);
+    setIncCompleateTodos(newTodos);
+  };
+
+  const onClickCompleate = (index) => {
+    const newInCompleateTodos = [...inCompleateTodos];
+    newInCompleateTodos.splice(index, 1);
+    setIncCompleateTodos(newInCompleateTodos);
+
+    const newTodos = [...compleateTodos, inCompleateTodos[index]];
+    setCompleateTodos(newTodos);
+  };
+
+  const onClickBack = (index) => {
+    const newCompleateTodos = [...compleateTodos];
+    newCompleateTodos.splice(index, 1);
+    setCompleateTodos(newCompleateTodos);
+
+    const newTodos = [...inCompleateTodos, compleateTodos[index]];
+    setIncCompleateTodos(newTodos);
+  };
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incompleate-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          <li className="list-row">
-            <p>AAAA</p>
-            <button>完了</button>
-            <button>削除</button>
-          </li>
-          <li className="list-row">
-            <p>BBBB</p>
-            <button>完了</button>
-            <button>削除</button>
-          </li>
+          {inCompleateTodos.map((todo, index) => {
+            return (
+              <li key={todo} className="list-row">
+                <p>{todo}</p>
+                <button onClick={() => onClickCompleate(index)}>完了</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div className="compleate-area">
         <p className="title">完了のTODO</p>
         <ul>
-          <li className="list-row">
-            <p>CCCC</p>
-            <button>戻す</button>
-          </li>
+          {compleateTodos.map((todo, index) => {
+            return (
+              <li key={todo} className="list-row">
+                <p>{todo}</p>
+                <button onClick={() => onClickBack(index)}>戻す</button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </>
